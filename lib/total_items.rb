110bdd5dd5c6e19d7565d997291829913d3222d1
Andrew Cookson <acookson91@gmail.com>
2016-06-01 17:19:14 +0100

    refactored calculation code into private method

--- lib/total_items.rb
+++ lib/total_items.rb
@@ -1,6 +1,12 @@
 class TotalItems
 
   def calculate(items)
+    @total = calculate_subtotal(items)
+  end
+
+  private
+
+  def calculate_subtotal(items)
     new_subtotal = 0
     items.each {|x| new_subtotal += x[:price].to_f}
     new_subtotal
