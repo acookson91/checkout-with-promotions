da5c8ab135174c59aaf5ffeea11a7ee1baa47453
Andrew Cookson <acookson91@gmail.com>
2016-06-01 16:44:52 +0100

    Added test to check that items will be added to array

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -3,9 +3,16 @@ require 'basket'
 describe Basket do
 
   subject(:basket){described_class.new}
+
   context 'stores items' do
     it 'starts with a empty item array' do
       expect(basket.items).to eq([])
+    end
+
+    it 'stores items into array' do
+      item_info = [{product_code: '001',name:'Lavender heart', price:'9.25'}]
+      basket.store(item_info)
+      expect(basket.items).to eq(item_info)
     end
   end
 
