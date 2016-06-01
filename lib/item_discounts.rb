ea9bb58b9a26f0b80f879a741492c2175911b6c0
Andrew Cookson <acookson91@gmail.com>
2016-06-01 20:26:15 +0100

    mutibuy discount test passing now but needs to be refactored

--- lib/item_discounts.rb
+++ lib/item_discounts.rb
@@ -1,4 +1,15 @@
 class ItemDiscounts
 
+  def percent_off(subtotal,limit,percentage)
+    if subtotal >= limit
+      percentage/100.to_f * subtotal
+    end
+  end
+
+  def multibuy(items,product_code,quantity,discount)
+    if items.select {|x| x[:product_code] == product_code}.count >= quantity
+      items.select {|x| x[:product_code] == product_code}.count * discount
+    end
+  end
 
 end
--- spec/item_discounts_spec.rb
+++ spec/item_discounts_spec.rb
@@ -8,4 +8,14 @@ describe ItemDiscounts do
     expect(itemdiscounts.percent_off(100,60,20)).to eq(20)
   end
 
+  it 'calculates multibuy discount' do
+    items1 = {product_code: '001',name:'Lavender heart', price:'9.25'}
+    items2 = {product_code: '001',name:'Lavender heart', price:'9.25'}
+    items3 = {product_code: '002',name:'Personalised cufflinks', price:'45'}
+    items = [items1,items2,items3]
+    product_code = '001'
+    quantity = 2
+    discount = 2
+    expect(itemdiscounts.multibuy(items,product_code,quantity,discount)).to eq(4)
+  end
 end
