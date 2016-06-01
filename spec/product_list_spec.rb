90b107ab441d254fef17f4ea7208527dfe83ec9a
Andrew Cookson <acookson91@gmail.com>
2016-06-01 16:04:42 +0100

    added test for product list, to check if the product is vaild

---
+++ spec/product_list_spec.rb
@@ -0,0 +1,14 @@
+require 'product_list'
+
+describe ProductList do
+
+  subject(:productlist){described_class.new}
+
+  context 'check product' do
+    it 'raises error if item is not avalaible' do
+      item_code = '004'
+      expect{product_list.find_item(item_code)}.to raise_error('There is no data on this item')
+    end
+  end
+
+end
