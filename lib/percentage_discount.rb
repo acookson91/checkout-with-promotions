12e0e80bd941239768138ead37b9482106c63627
Andrew Cookson <acookson91@gmail.com>
2016-06-04 18:10:56 +0100

    tests now passing for percentage discount class

--- lib/percentage_discount.rb
+++ lib/percentage_discount.rb
@@ -1,3 +1,20 @@
 class PercentageDiscount
 
+  def initialize(limit,percentage)
+    @limit = limit
+    @percentage = percentage
+  end
+
+  def calculate_discount(basket)
+    if basket.request_subtotal >= @limit
+      percent(@percentage) * basket.request_subtotal
+    end
+  end
+
+  private
+
+  def percent(percentage)
+    percentage/100.to_f
+  end
+
 end
--- spec/percentage_discount_spec.rb
+++ spec/percentage_discount_spec.rb
@@ -4,11 +4,15 @@ describe PercentageDiscount do
 
   limit = 50
   percentage = 50
-  subject(percentage_discount){described_class.new(limit,percentage)}
+  subject(:percentage_discount){described_class.new(limit,percentage)}
   let(:basket){double:basket, request_subtotal: 60}
 
-  it 'calculates 10% discount' do
-    expect(percentage_discount.calculate_discount(basket)).to eq(20)
+  context 'percentage discount' do
+
+    it 'calculates 10% discount if limit mate' do
+      expect(percentage_discount.calculate_discount(basket)).to eq(30)
+    end
+
   end
 
 end
